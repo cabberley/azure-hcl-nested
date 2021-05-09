dd906dbb1f4c2a234b037afcb54c2f8cc4d87d83
Daniel Scholl <dascholl@microsoft.com>
2021-05-09 18:47:12 -0500

    Updated

--- scripts/createGroup.sh
+++ scripts/createGroup.sh
@@ -1,5 +1,46 @@
 #!/usr/bin/env bash
 #
+#  Purpose: Create and Lock a resource group.
+#  Usage:
+#    createGroup.sh
+
+###############################
+## ARGUMENT INPUT            ##
+###############################
+
+if [ ! -z $1 ]; then RESOURCEGROUP=$1; else echo "RESOURCEGROUP not found." && exit 1; fi
+
+if [ -z "$Location" ]; then echo "Location not found." && exit 1; fi
+
+# Get commandline for Azure CLI
+az=$(which az)
+
+echo "================================================================================="
+echo "Creating a Resource Group"
+$az group create --name $RESOURCEGROUP --location $location -ojsonc
+sleep 3
+echo "================================================================================="
+
+exit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/usr/bin/env bash
+#
 #  Purpose: Create a Resource Group and Lock
 #  Usage:
 #    createGroup.sh
