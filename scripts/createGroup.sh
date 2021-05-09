087d4d79ae8a5705d8615300c27100c042886122
Daniel Scholl <dascholl@microsoft.com>
2021-05-09 17:21:46 -0500

    Updated

--- scripts/createGroup.sh
+++ scripts/createGroup.sh
@@ -18,7 +18,7 @@ echo "==========================================================================
 if [ ! "$($az group show -n $RESOURCEGROUP --query tags.currentStatus -o tsv 2>/dev/null)" = "groupCreated" ]; then
     # Deploy the resource group and update Status Tag
     echo "Creating a resource group."
-    $az group create -g "$RESOURCEGROUP" -l "$LOCATION" -o none > /dev/null 2>&1
+    $az group create -g "$RESOURCEGROUP" -l "$Location" -o none > /dev/null 2>&1
     $az group lock create --lock-type CanNotDelete -n "DontDelete" -g $RESOURCEGROUP
     echo "Resource Group: " $RESOURCEGROUP
 fi
