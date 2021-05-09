69255d61985f146028e034577d2a8851406a12d7
Daniel Scholl <dascholl@microsoft.com>
2021-05-09 18:31:23 -0500

    Updated

--- scripts/createGroup.sh
+++ scripts/createGroup.sh
@@ -9,16 +9,23 @@
 ###############################
 
 if [ ! -z $1 ]; then RESOURCEGROUP=$1; else echo "RESOURCEGROUP not found." && exit 1; fi
-
 if [ -z "$Location" ]; then echo "Location not found." && exit 1; fi
 
+# Get commandline for Azure CLI
+az=$(which az)
+
+env
 
 # Create Resource Group
 echo "================================================================================================================="
-if [ ! "$($az group show -n $RESOURCEGROUP --query tags.currentStatus -o tsv 2>/dev/null)" = "groupCreated" ]; then
-    # Deploy the resource group and update Status Tag
-    echo "Creating a resource group."
-    $az group create -g "$RESOURCEGROUP" -l "$Location" -o none > /dev/null 2>&1
-    $az group lock create --lock-type CanNotDelete -n "DontDelete" -g $RESOURCEGROUP
-    echo "Resource Group: " $RESOURCEGROUP
+local _result=$($az group show --name $RESOURCEGROUP)
+if [ "$_result"  == "" ]
+then
+  OUTPUT=$($az group create --name $RESOURCEGROUP \
+    --location $location \
+    -ojsonc)
+  $az group lock create --lock-type CanNotDelete -n "DontDelete" -g $RESOURCEGROUP
+  echo "Resource Group: " $RESOURCEGROUP
+else
+  echo "Resource Group $RESOURCEGROUP already exists."
 fi
