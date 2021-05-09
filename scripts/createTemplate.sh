90cb65ecb0dc2cb4c121d0b166110fd47fb3edd8
Daniel Scholl <dascholl@microsoft.com>
2021-05-09 10:26:05 -0500

    Updated

--- scripts/createTemplate.sh
+++ scripts/createTemplate.sh
@@ -10,17 +10,16 @@
 
 if [ ! -z $1 ]; then TEMPLATE_URL=$1; else echo "TEMPLATE_URL not found." && exit 1; fi
 
-if [ -z "$LOCATION" ]; then echo "LOCATION not found." && exit 1; fi
+if [ -z "$Location" ]; then echo "Location not found." && exit 1; fi
 if [ -z "$RESOURCEGROUP" ]; then echo "RESOURCEGROUP not found." && exit 1; fi
 
 # Get commandline for Azure CLI
 az=$(which az)
 
-
 echo "================================================================================="
 echo "Creating the solution template"
 wget $TEMPLATE_URL -O templateSpec.json > /dev/null 2>&1
 sleep 3
-$az ts create --name "edgeSolution"  --resource-group $RESOURCEGROUP --location $LOCATION --version "1.0" --template-file "./templateSpec.json" -o none 2>/dev/null
+$az ts create --name "edgeSolution"  --resource-group $RESOURCEGROUP --location $Location --version "1.0" --template-file "./templateSpec.json" -o none 2>/dev/null
 $az group update -n $RESOURCEGROUP --tag currentStatus=templateCreated > /dev/null 2>&1
 echo "================================================================================="
